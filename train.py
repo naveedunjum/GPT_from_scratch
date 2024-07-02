7ff852588b345627be0d47214971edc629a76951
Naveed Unjum <naveed.unjum97@gmail.com>
2024-07-02 12:19:23 +0200

    add multihead, feedforward and dropout

--- train.py
+++ train.py
@@ -74,8 +74,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8D self attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -85,8 +85,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # (B,T,C), C = n_embd
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # T, C
         x = tok_emb + pos_emb  # (B,T,C)
-        x = self.sa_heads(x)
-        x = self.ffwd(x) # (B,T,C))
+        x = self.blocks(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
 
@@ -149,22 +148,46 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size) -> None:
         super().__init__()
         self.heads = (nn.ModuleList([Head(head_size) for _ in range(num_heads)]))
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
- 
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+    
+
 class FeedForward(nn.Module):
 
     def __init__(self, n_embd) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
+
     
     def forward(self, x):
         return self.net(x)
 
+class Block(nn.Module):
+    """Transformer block"""
+
+    def __init__(self, n_embd, n_head) -> None:
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        # Normalise the features, 
+        self.ln1 = nn.LayerNorm(n_embd) 
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))  
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 
 xb, yb = get_batch("train")
